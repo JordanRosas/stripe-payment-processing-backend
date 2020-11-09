712d1c673abf2d2c3c421fd05509864d67f0b8e8
Jordan Rosas <jordanrosas1203@gmail.com>
2020-11-08 22:04:39 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const cors = require("cors")
+const express = require("express")
+const stripe = require("stripe")("sk_test_51HFVOJIfXEGU7Vl42GvIwQEo19ONDkvOcBmiQJzCBbYQC9nMSHyhV4nyhhPbjsK68FwVC5Hfq4VjB2xRqzFlFlcx00zOYPU5zx")
+const { v4: uuidV4 } = require('uuid');
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//routes
+app.get("/", (req, res) => {
+    res.send("Stripe tutorial working")
+})
+
+app.post("/payment", (req, res) => {
+    const {product, token} = req.body
+
+    console.log("PRODUCT", product)
+    console.log("PRICE", product.price)
+    //Make sure the user isn't charged twice
+    const idempotencyKey = uuidV4()
+    
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer:customer.id,
+            receipt_email: token.email,
+            description:product.name,
+        }, {idempotencyKey})
+    }).then(result => res.status(200).json(result))
+    .catch(err => console.log(err))
+
+})
+//listener
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log('listening @ port ' + port ))
